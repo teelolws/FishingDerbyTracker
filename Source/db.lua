3bfb7924ce22c1386050392302b5e5a07591ab92
Taizun <15648000+Taizunz@users.noreply.github.com>
2025-10-11 16:05:56 +0200

    Update db.lua
    Rearranged a lot of the data, sorted it all alphabetically in each zone.
    All of this is based on personal observations from doing this Derby every week since TWW release and data from Wowhead.
    Some of the data in the in-game journal entries isn't entirely true, which is a classic Blizzard moment.

--- Source/db.lua
+++ Source/db.lua
@@ -4,43 +4,53 @@ addon.zones = {}
 
 addon.zones[1] = { -- isleofdorn
     2248,
-    {name = "Bloody Perch", quests = {82918}, info = "- Festering Rotpool" },
-	{name = "Cursed Ghoulfish", quests = {82935}, info = "- Festering Rotpool" },
-	{name = "DillyDally Dace", quests = {82947}, info = "- Festering Rotpool" },
-	{name = "Pale Huskfish", quests = {82931}, info = "- Festering Rotpool" },
-	{name = "Quiet River Bass", quests = {82925}, info = "- Calm Surfacing Ripple" },
+	{name = "Cursed Ghoulfish", quests = {82935}, info = "- Any pool (highest chance in Festering Rotpool)" },
+	{name = "Dornish Pike", quests = {82926}, info = "- Calm Surfacing Ripple" },
+	{name = "Goldengill Trout", quests = {82923}, info = "- Glimmerpool & Festering Rotpool" },
+	{name = "Pale Huskfish", quests = {82931}, info = "- Festering Rotpool & Infused Ichor Spill" },
+	{name = "Spiked Sea Raven", quests = {82933}, info = "- Stargazer Swarm (highest chance) & Calm Surfacing Ripple & Anglerseeker Torrent" },
 	{name = "Whispering Stargazer", quests = {82922}, info = "- Stargazer Swarm" },
-
 }
 
 addon.zones[2] = { -- ringingdeeps
     2214,
-	{name = "Quiet River Bass", quests = {82925}, info = "- Calm Surfacing Ripple & River Bass Pool" },
 	{name = "Roaring Anglerseeker", quests = {82927}, info = "- Anglerseeker Torrent" },
 }
+
 addon.zones[3] = { -- hallowfall
     2215,
-    {name = "Arathor Hammerfish", quests = {82928}, info = "- Blood In Water" },
-    {name = "Kaheti Slum Shark", quests = {82930}, info = "- Blood In Water" },
-    {name = "Roaring Anglerseeker", quests = {82927}, info = "- Anglerseeker Torrent" },
-    {name = "Sanguine Dogfish", quests = {82932}, info = "- Blood In Water" },
+    {name = "Arathor Hammerfish", quests = {82928}, info = "- Blood in the Water (Use Arathor Hammerfish Lure for increased chance)" },
+	{name = "Cursed Ghoulfish", quests = {82935}, info = "- Any pool (highest chance in Festering Rotpool)" },
+	{name = "Dornish Pike", quests = {82926}, info = "- Calm Surfacing Ripple" },
+	{name = "Goldengill Trout", quests = {82923}, info = "- Glimmerpool & Festering Rotpool" },
+    {name = "Kaheti Slum Shark", quests = {82930}, info = "- Blood in the Water & Swarm of Slum Sharks" },
+	{name = "Pale Huskfish", quests = {82931}, info = "- Festering Rotpool & Infused Ichor Spill" },
+	{name = "Queen's Lurefish", quests = {82934}, info = "- Throw in Regal Dottyback for buff, then fish anywhere in Hallowfall or Azj-Kahet" },
+	{name = "Regal Dottyback", quests = {82929}, info = "- Royal Ripple" },
+	{name = "Roaring Anglerseeker", quests = {82927}, info = "- Anglerseeker Torrent" },
+    {name = "Sanguine Dogfish", quests = {82932}, info = "- Blood in the Water (Or throw in Bloody Perch for increased chance and fish anywhere)" },
+	{name = "Spiked Sea Raven", quests = {82933}, info = "- Stargazer Swarm (highest chance) & Calm Surfacing Ripple & Anglerseeker Torrent" },
 }
 
 addon.zones[4] = { -- azjkahet
     2255,
-    {name = "Awoken Coelacanth", quests = {82936}, info = "- Throw in 10 Stargazer Fish & then fish anywhere in the zone"},
-    {name = "Queen Lurefish", quests = {82934}, info = "- Throw in Regal Dottyback Fish for Buff & then fish anywhere in the zone"},
+	{name = "Awoken Coelacanth", quests = {82936}, info = "- Throw in 10 Whispering Stargazer, then fish anywhere in the zone" },
+	{name = "Cursed Ghoulfish", quests = {82935}, info = "- Any pool (highest chance in Festering Rotpool)" },
+	{name = "Dornish Pike", quests = {82926}, info = "- Calm Surfacing Ripple" },
+	{name = "Goldengill Trout", quests = {82923}, info = "- Glimmerpool & Festering Rotpool" },
+	{name = "Kaheti Slum Shark", quests = {82930}, info = "- Blood in the Water & Swarm of Slum Sharks" },
+	{name = "Pale Huskfish", quests = {82931}, info = "- Festering Rotpool & Infused Ichor Spill" },
+    {name = "Queen's Lurefish", quests = {82934}, info = "- Throw in Regal Dottyback for buff, then fish anywhere in Hallowfall or Azj-Kahet" },
+	{name = "Regal Dottyback", quests = {82929}, info = "- Royal Ripple" },
 }
 
 addon.zones[5] = { -- any
     2274,
 	{name = "Bismuth Bitterling", quests = {82920}, info = "- Glimmerpool" },
-	{name = "Crystalline Sturgeion", quests = {82919}, info = "- Glimmerpool" },
-    {name = "Dornish Pike", quests = {82926}, info = "- Calm Surfacing Ripple" },
-	{name = "Goldengill Trout", quests = {82923}, info = "- Glimmerpool" },
+	{name = "Bloody Perch", quests = {82918}, info = "- Bloody Perch Swarm & Calm Surfacing Ripple & Festering Rotpool & Blood in the Water & Swarm of Slum Sharks" },
+	{name = "Crystalline Sturgeon", quests = {82919}, info = "- Glimmerpool" },
+	{name = "Dilly-Dally Dace", quests = {82947}, info = "- Calm Surfacing Ripple & Blood in the Water & Festering Rotpool" },
 	{name = "Nibbling Minnow", quests = {82921}, info = "- Calm Surfacing Ripple" },
-	{name = "Regal Dottyback", quests = {82929}, info = "- Calm Surfacing Ripple & Royal Ripple"},
-    {name = "Specular Rainbowfish", quests = {82924}, info = "- Glimmerpool" },
-	{name = "Spike Sea Raven", quests = {82933}, info = "- Any Pool" },
+	{name = "Quiet River Bass", quests = {82925}, info = "- River Bass Pool & Calm Surfacing Ripple" },
+    {name = "Specular Rainbowfish", quests = {82924}, info = "- Glimmerpool (Use Specular Rainbowfish Lure for increased chance)" },
 }
-
